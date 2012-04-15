2702ac7af571e3e8f4a9280cb6ff76474c6773df
Rich Kuo <richard.p.kuo@gmail.com>
2012-04-15 13:30:22 -0700

    added pages

---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get brogramming" do
+    get :brogramming
+    assert_response :success
+  end
+
+  test "should get health/fitness" do
+    get :health/fitness
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+end
