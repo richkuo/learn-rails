2d7c1d5af2d515f1e2c1771318b4aa029161ecf8
Rich Kuo <richard.p.kuo@gmail.com>
2012-04-15 20:25:02 -0700

    added css stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+
+   def full_title(page_title)
+      default_title = "richkuo.com"
+      if page_title.empty?
+         default_title
+      else
+         "#{default_title} | #{page_title}"
+      end
+   end
+
 end
