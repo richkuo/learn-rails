7e72bed83e0fd34dc4ccf3041bb1886b97c2b424
Rich Kuo <richard.p.kuo@gmail.com>
2012-05-03 06:45:06 -0700

    added name and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@
 
 class User < ActiveRecord::Base
    attr_accessible :name, :email
+
+   validates :name, presence: true, length: { maximum: 45 }
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   validates :email, presence: true,
+                     length: { maximum: 45 },
+                     format: { with: VALID_EMAIL_REGEX },
+                     uniqueness: { case_sensitive: false }
 end
