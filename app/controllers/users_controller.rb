b20bff7848f76ae82bbe2c282d596525d0bcdf78
Rich Kuo <richard.p.kuo@gmail.com>
2012-06-19 07:48:27 -0400

    added stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
-  
+   def index
+   end
+   
+   def create
+     @user = User.new(params[:user])
+     if @user.save
+       flash[:success] = "Welcome!"
+       redirect_to @user
+     else
+       render 'new'
+     end
+   end
+
+
    def show
       @user = User.find(params[:id])
    end
