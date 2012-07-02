a998111bb4f240d06e48cb17f2b3c917fe745ecf
Rich Kuo <richard.p.kuo@gmail.com>
2012-07-02 18:56:23 -0400

    paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+   before_filter :signed_in_user, only: [:index, :edit, :update]
+   before_filter :correct_user, only: [:edit, :update]
+   
    def index
+     @users = User.paginate(page: params[:page])
    end
    
    def show
@@ -20,4 +24,34 @@ class UsersController < ApplicationController
        render 'new'
      end
    end
+   
+   def edit
+     @user = User.find(params[:id])
+   end
+
+   def update
+     @user = User.find(params[:id])
+     if @user.update_attributes(params[:user])
+       # Handle a successful update.
+       flash[:success] = "Profile successfully updated!"
+       sign_in @user
+       redirect_to @user
+     else
+       render 'edit'
+     end
+   end
+
+   private
+
+     def signed_in_user
+       unless signed_in?
+         store_location
+         redirect_to signin_path, notice: "Please sign in."
+       end
+     end
+
+     def correct_user
+       @user = User.find(params[:id])
+       redirect_to(root_path) unless current_user?(@user)
+     end
 end
