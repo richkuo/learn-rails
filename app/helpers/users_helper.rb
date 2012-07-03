270cd11e0f3931948685b1036d289144f4e25bc9
Rich Kuo <richard.p.kuo@gmail.com>
2012-07-03 03:42:15 -0400

    changed some headers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
 
-   def gravatar_for(user, options = { size: 50})
+   def gravatar_for(user, options = { size: 100})
       gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
       size = options[:size]
-      gravatar_url = "html://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
       image_tag(gravatar_url, alt: user.name, class: "gravatar")
    end
    
