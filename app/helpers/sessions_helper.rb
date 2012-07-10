b7eec541814139ae8670573fba8ad6b4704bed98
Rich Kuo <richard.p.kuo@gmail.com>
2012-07-10 16:08:05 -0400

    fixed another bug

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,7 +24,7 @@ module SessionsHelper
   def signed_in_user
     unless signed_in?
       store_location
-      redirect_to signin_path, notice: "Please sign in."
+      redirect_to signin_path, notice: "This page requires you to sign in."
     end
   end
 
